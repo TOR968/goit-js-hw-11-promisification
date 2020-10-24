ff9268985aafb0c382f70209fd955c7a4de24f00
Maxim Turbal <tor968@yahoo.com>
2020-10-24 16:07:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-const delay = ms => {
-  // Твой код
-};
-
-const logger = time => console.log(`Resolved after ${time}ms`);
-
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+import './task1';
+import './task2';
+import './task3';
